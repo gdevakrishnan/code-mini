a4f257416f4b4b6b089c22ec693ea857f5be71fa
gdevakrishnan <gdevakrishnan700@gmail.com>
2024-07-26 15:23:03 +0530

    User auth navigate and navbar profile name

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "api_key",
-  authDomain: "auth_domain",
-  projectId: "project_id",
-  storageBucket: "storage_bucket",
-  messagingSenderId: "messaging_sender_id",
-  appId: "app_id"
+  apiKey: "AIzaSyClLymWXGcykBsI2KAcr3onGeSVXGNpL5A",
+  authDomain: "code-mini-519a4.firebaseapp.com",
+  projectId: "code-mini-519a4",
+  storageBucket: "code-mini-519a4.appspot.com",
+  messagingSenderId: "154485541411",
+  appId: "1:154485541411:web:4d4f5d496f52b35d15ff96"
 };
 
 export default firebaseConfig;
