e5d9484d6527de160c627a244878dbeec650d756
gdevakrishnan <gdevakrishnan700@gmail.com>
2024-08-11 08:43:44 +0530

    Update firebase config

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyClLymWXGcykBsI2KAcr3onGeSVXGNpL5A",
-  authDomain: "code-mini-519a4.firebaseapp.com",
-  projectId: "code-mini-519a4",
-  storageBucket: "code-mini-519a4.appspot.com",
-  messagingSenderId: "154485541411",
-  appId: "1:154485541411:web:4d4f5d496f52b35d15ff96"
+  apiKey: "YOUR API KEY",
+  authDomain: "YOUR AUTH DOMAIN",
+  projectId: "YOUR PROJECT ID",
+  storageBucket: "YOUR STORAGE BUCKET",
+  messagingSenderId: "YOUR MESSAGING SENDER ID",
+  appId: "YOUR APP ID"
 };
 
 export default firebaseConfig;
