1aa081ecae956a8ce86ef1345cd919d35bd46f7a
gdevakrishnan <gdevakrishnan700@gmail.com>
2024-08-11 21:24:56 +0530

    Dashboard get started button

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,14 +1,22 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useContext } from 'react'
 import Features from '../components/Features'
+import { Link } from 'react-router-dom'
+import appContext from '../context/appContext'
 
 function Dashboard() {
+  const {
+    user
+  } = useContext(appContext);
+
   return (
     <Fragment>
       <section className="hero hero_page">
         <div className="bg"></div>
         <h1 className="hero_title">AI for every programmers</h1>
         <p className="hero_sub_title">Build with Google Gemini to make <span>helpful for everyone</span></p>
-        <button className="btn">Get Started</button>
+        <Link to={(user && user.uname && user.gmail) ? '/code' : '/signin'}>
+          <button className="btn">Get Started</button>
+        </Link>
       </section>
       <Features />
     </Fragment>
